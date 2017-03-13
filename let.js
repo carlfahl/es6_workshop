3495eafbad6b286a3ea5aeaa4cdbc834d5026c8d
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-03-13 15:49:49 -0600

    added examples of let and const usage

---
+++ let.js
@@ -0,0 +1,42 @@
+// Example of block level scope with the let keyword
+
+function test () {
+  var x = 10;
+
+  for (let i=0; i<5; i++) {
+    console.log(i);
+  }
+
+  for (var j=0; j<5; j++) {
+    console.log(j);
+  }
+
+  console.log("now j is ", j);
+  console.log(x);
+  try {
+    console.log("now i is ", i);  // this causes an error, i is not defined
+  }
+  catch (e) {
+    if (e instanceof ReferenceError) {
+      console.log('i is not defined.');
+    }
+  }
+}
+
+test();
+
+function constTest() {
+  // const variables are read only and block scoped.
+  const x = 10;
+
+  if (true) {
+    const x = 10;
+    const y = 15;
+    const z = 4;
+  }
+
+  console.log(z); // z is not defined here.
+  x = 12; // cannot reassign to a const variable.
+}
+
+constTest();
